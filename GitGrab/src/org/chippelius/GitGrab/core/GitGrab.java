ebbdbaa428b7756b175f514075c39d1f09190365
Leo Köberlein <leo@wolfgang-koeberlein.de>
2019-06-10 00:46:22 +0200

    WIP Sync-Commit

--- GitGrab/src/org/chippelius/GitGrab/core/GitGrab.java
+++ GitGrab/src/org/chippelius/GitGrab/core/GitGrab.java
@@ -24,15 +24,7 @@ public class GitGrab {
 	// ******************************
 	private static final String latestVersion = "latest";
 	
-	/*
-	public static final int OWNER_ARGUMENT = 1;
-	public static final int REPO_ARGUMENT = 2;
-	public static final int ASSET_ARGUMENT = 4;
-	public static final int DIRECTORY_ARGUMENT = 8;
-	public static final int VERSION_ARGUMENT = 16;
-	public static final int ALLOW_OVERRIDE_ARGUMENT = 32;
-	*/
-
+	
 
 
 	// ******************************
@@ -40,7 +32,8 @@ public class GitGrab {
 	// ******************************
 	protected GitGrabUI ui;
 //	protected volatile boolean abort;
-	protected String repoOwner, repoName, version, assetRegex;
+	protected String repoOwner, repoName, version, asset, destination;
+	protected boolean override;
 
 
 
@@ -66,17 +59,29 @@ public class GitGrab {
 
 
 
-/*
 	// ******************************
 	// Private methods
 	// ******************************
-	private String getValidStringArgument(int argrument, String currentValue) {
-		while((currentValue == null || currentValue.equals("")) && !abort) {
-			currentValue = ui.requestStringArgument(argrument, currentValue);
-		}
-		return currentValue;
+	protected static URLConnection getURLConnection(String url) throws IOException {
+		URL u = new URL(url);
+		URLConnection uc = u.openConnection();
+		uc.setRequestProperty("User-Agent", "Chippelius/GitGrab");
+		uc.setRequestProperty("Accept", "application/vnd.github.v3+json");
+		return uc;
 	}
-
+	
+	protected static void delete(File file) {
+		if(file.exists()) {
+			if(file.isDirectory()) {
+				for(File f : file.listFiles()) {
+					delete(f);
+				}
+			}
+			file.delete();
+		}
+	}
+	
+/*
 	private String getPackageURL(String owner, String repo, String version, String asset) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		URL url = new URL("https://api.github.com/repos/"+owner+"/"+repo+"/releases/"+version);
@@ -91,22 +96,12 @@ public class GitGrab {
 
 		return res;
 	}
+*/
 
-	  */
-
-
-
-	
-	
-	
-	
-	
-	protected static URLConnection getURLConnection(String url) throws IOException {
-		URL u = new URL(url);
-		URLConnection uc = u.openConnection();
-		uc.setRequestProperty("User-Agent", "Chippelius/GitGrab");
-		uc.setRequestProperty("Accept", "application/vnd.github.v3+json");
-		return uc;
+	protected static void unzip(String zipFile, String destinationDirectory) throws Exception {
+		//TODO: redo without dependency
+		ZipFile zipfile = new ZipFile(zipFile);
+		zipfile.extractAll(destinationDirectory);
 	}
 	
 	
@@ -115,100 +110,108 @@ public class GitGrab {
 	
 	
 	// ******************************
-	// Public methods
+	// Getter and Setter methods
 	// ******************************
 
-//	public void abort() {
-//		abort = true;
-//	}
-
-	/**
-	 * @return the repoOwner
-	 */
 	public synchronized String getRepoOwner() {
 		return repoOwner;
 	}
-	/**
-	 * @param repoOwner the repoOwner to set
-	 */
 	public synchronized GitGrab setRepoOwner(String repoOwner) {
 		this.repoOwner = repoOwner;
 		return this;
 	}
 
-	/**
-	 * @return the repoName
-	 */
 	public synchronized String getRepoName() {
 		return repoName;
 	}
-	/**
-	 * @param repoName the repoName to set
-	 */
 	public synchronized GitGrab setRepoName(String repoName) {
 		this.repoName = repoName;
 		return this;
 	}
 
-	/**
-	 * @return the version
-	 */
 	public synchronized String getVersion() {
 		return version;
 	}
-	/**
-	 * @param version the version to set
-	 */
 	public synchronized GitGrab setVersion(String version) {
 		this.version = version;
 		return this;
 	}
 
-	/**
-	 * @return the assetRegex
-	 */
-	public synchronized String getAssetRegex() {
-		return assetRegex;
+	public synchronized String getAsset() {
+		return asset;
 	}
-	/**
-	 * @param assetRegex the assetRegex to set
-	 */
-	public synchronized GitGrab setAssetRegex(String assetRegex) {
-		this.assetRegex = assetRegex;
+	public synchronized GitGrab setAsset(String asset) {
+		this.asset = asset;
+		return this;
+	}
+
+	public synchronized String getDestination() {
+		return destination;
+	}
+	public synchronized GitGrab setDestination(String destination) {
+		this.destination = destination;
+		return this;
+	}
+
+	public synchronized boolean isOverride() {
+		return override;
+	}
+	public synchronized GitGrab setOverride(boolean override) {
+		this.override = override;
 		return this;
 	}
 
 
 
+
+	// ******************************
+	// Public methods
+	// ******************************
 
 	public synchronized void install() throws Exception {
 		JsonReader reader = Json.createReader(getURLConnection("https://api.github.com/repos/"+repoOwner+"/"+repoName+"/releases/"+version).getInputStream());
 		JsonObject root = reader.readObject();
 		JsonArray assets = root.getJsonArray("assets");
 		for(int i=0; i<assets.size(); ++i) {
-			if(assets.getJsonObject(i).getString("name").matches(assetRegex)) {
+			if(assets.getJsonObject(i).getString("name").equals(asset)) {
 				System.out.println("Downloading "+assets.getJsonObject(i).getString("name"));
 				URLConnection downloadConnection = getURLConnection(assets.getJsonObject(i).getString("url"));
 				downloadConnection.setRequestProperty("Accept", "application/octet-stream");
 				InputStream in = downloadConnection.getInputStream();
-				FileOutputStream fos = new FileOutputStream("C:\\Users\\Leo\\Downloads\\"+assets.getJsonObject(i).getString("name"), false);
+				FileOutputStream fos = new FileOutputStream(destination+assets.getJsonObject(i).getString("name"), false);
 				byte[] buffer = new byte[1024];
 				for(int count=0;(count=in.read(buffer)) > -1; fos.write(buffer, 0, count));
 				in.close();
 				fos.close();
 				System.out.println("Download complete.");
 				
+				File destinationFolder = new File(destination);
+				if(destinationFolder.exists()) {
+					if(override) {
+						delete(destinationFolder);
+						destinationFolder.mkdir();
+					} else {
+						System.out.println("Destination already exists!");
+						return;
+					}
+				} else {
+					destinationFolder.mkdirs();
+				}
+				
 				System.out.println("Extracting "+assets.getJsonObject(i).getString("name"));
-				ZipFile zipfile = new ZipFile("C:\\Users\\Leo\\Downloads\\"+assets.getJsonObject(i).getString("name"));
-				zipfile.extractAll("C:\\Users\\Leo\\Downloads\\");
+				unzip(destination+assets.getJsonObject(i).getString("name"), destination);
 				System.out.println("Extracting complete.");
 				
-				new File("C:\\Users\\Leo\\Downloads\\"+assets.getJsonObject(i).getString("name")).delete();
+				new File(destination+assets.getJsonObject(i).getString("name")).delete();
 				break;
 			}
 		}
 	}
 	
+//	public void abort() {
+//		abort = true;
+//	}
+
 	
 	
 
@@ -223,7 +226,14 @@ public class GitGrab {
 				public void init() {
 					// TODO Auto-generated method stub
 				}
-			}).setRepoOwner("atom").setRepoName("atom").setVersion(latestVersion).setAssetRegex(".*windows.*\\p{Punct}zip").install();
+			})
+			.setRepoOwner("atom")
+			.setRepoName("atom")
+			.setVersion(latestVersion)
+			.setAsset("atom-windows.zip")
+			.setDestination("C:\\Users\\leo\\Downloads\\atom\\")
+			.setOverride(false)
+			.install();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
